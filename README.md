65bba3acc1f3b159be8daf61a2341ba8ca0d4708
HanWol <hannahwolfson05@gmail.com>
2023-06-06 18:17:36 -0500

    created initial text/shape modules, installed relevant packages, wrote inquirer prompts, wrote functions for code generation based on shape/text inputs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # svggen
+Resources:
+- https://thenewcode.com/1037/SVG-Shape-Elements-Polygons code for equilateral triangle
--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+const Shapes = require('./lib/shapes.js');
+const logoText = require('./lib/text.js');
+// const svgFile = require('./svg.js')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const jest = require('jest');
 
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter logo text: ',
+        name: 'logotext'
+    },
+    {
+        type: 'input',
+        message: 'Text color (color name or hex code): ',
+        name: 'textcolor'
+    },
+    {
+        type: 'list',
+        message: 'Select a shape: ',
+        choices: ["Circle", "Square", "Triangle"],
+        name: 'shape'
+    },
+    {
+        type: 'input',
+        message: 'Shape color (color name or hex code): ',
+        name: 'shapecolor'
+    }
+]
+
+inquirer
+    .prompt(
+        questions
+    )
+
+    .then((data) => {
+        const getShape = Shapes(data);
+        const getText = logoText(data);
+        console.log(getShape, getText);
+    })
